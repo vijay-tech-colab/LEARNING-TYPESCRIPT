2d05c09c14a81e559682b372855a9d0b94fc46e8
vijay kumar <vijaykumar06232@gmail.com>
2024-10-27 11:30:35 +0530

    introduction ts update

---
+++ Day 1/first.js
@@ -0,0 +1,2 @@
+console.log('My first code of TypeScript !');
+console.log('I am learning Typescript');
---
+++ Day 1/first.ts
@@ -0,0 +1,3 @@
+console.log('My first code of TypeScript !');
+console.log('I am learning Typescript');
+
