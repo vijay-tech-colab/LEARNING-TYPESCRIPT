fa67346efa43e441df783045ca8455bfa297e579
vijay kumar <vijaykumar06232@gmail.com>
2024-11-05 09:13:16 +0530

    variable in typeScript

---
+++ variable/variable.js
@@ -0,0 +1,5 @@
+// ypeScript allows you to specify the type of a variable using a colon : followed by the type.
+var age = 25; // Variable with a number type
+var name1 = "John"; // Constant variable with a string type
+console.log(age);
+console.log(name1);
---
+++ variable/variable.ts
@@ -0,0 +1,6 @@
+// ypeScript allows you to specify the type of a variable using a colon : followed by the type.
+
+let age: number = 25;        // Variable with a number type
+const name1: string = "John"; // Constant variable with a string type
+console.log(age);
+console.log(name1);
