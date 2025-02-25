8f93b5df77a90b708d58e94f91650cd65e50a505
vijay kumar <vijaykumar06232@gmail.com>
2025-02-25 09:54:52 +0530

    data type and its type assign

---
+++ datatype and type assignment/app.js
@@ -0,0 +1,12 @@
+var num = 10;
+var str = "vijay";
+var isMatch = true;
+var person = {
+    name: "vijay",
+    age: 20,
+    email: "vijaykumar@gamil.com",
+    address: {
+        city: "jaunpur",
+        pincode: "222878"
+    }
+};
---
+++ datatype and type assignment/app.ts
@@ -0,0 +1,22 @@
+let num : number = 10;
+let str : string = "vijay";
+let isMatch : boolean = true
+
+
+const person : {
+    name : string,
+    age : number,
+    email : string,
+    address : {
+        city : string,
+        pincode : string
+    }
+} = {
+    name : "vijay",
+    age : 20,
+    email : "vijaykumar@gamil.com",
+    address : {
+        city : "jaunpur",
+        pincode : "222878"
+    }
+}
