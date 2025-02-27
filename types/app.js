bb155bdc78a2f655541e40fdc4662b74ef6e455b
vijay kumar <vijaykumar06232@gmail.com>
2025-02-27 07:41:41 +0530

    any literal and union type

---
+++ types/app.js
@@ -0,0 +1,9 @@
+// any type in typescript
+var anyTypesData = 9000;
+anyTypesData = "jhhkh";
+console.log(anyTypesData);
+var num = [];
+// union type in ts
+var str = {};
+// literal type in ts
+var data = "admin";
---
+++ types/app.ts
@@ -0,0 +1,19 @@
+// any type in typescript
+
+let anyTypesData: any = 9000;
+anyTypesData = "jhhkh";
+console.log(anyTypesData);
+
+
+let num : any [] = []
+
+
+// union type in ts
+
+let str : number | string | boolean | [] | {} = {}
+
+
+// literal type in ts
+
+let data : "admin" | "user" = "admin";
+ 
