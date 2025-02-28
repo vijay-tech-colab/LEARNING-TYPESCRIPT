72700a5c74e859ce5d6f646556d111e87b069fa3
vijay kumar <vijaykumar06232@gmail.com>
2025-02-28 06:56:39 +0530

    return type and type alias

---
+++ return_type and type_alias/app.ts
@@ -0,0 +1,14 @@
+// ! type alias 
+
+type strOrNum =  number;
+
+function add (num1 : strOrNum, num2 : strOrNum){
+    return num1 - num2
+}
+
+
+//  return type in ts
+
+function sayHello (str : string) : string {
+    return  str;
+}
